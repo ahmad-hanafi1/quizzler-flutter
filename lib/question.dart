d6741d763f1ebde96ecb7771e91f7e0d86227a7f
ahmad-hanafi1 <ahmadhanafi235@gmail.com>
2025-01-19 18:22:50 +0100

    modularized code

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = "";
+  bool questionAnswer = false;
+
+  Question(questionText, bool questionAnswer) {
+    this.questionText = questionText;
+    this.questionAnswer = questionAnswer;
+  }
+}
